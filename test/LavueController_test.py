223161ca3f97e7043c4f7b8b9bf66eb553280bea
Jan Kotanski <jankotan@gmail.com>
2018-12-14 10:18:04 +0100

    add tests and Init fix

--- test/LavueController_test.py
+++ test/LavueController_test.py
@@ -18,12 +18,32 @@ import unittest
 import PyTango
 import numpy as np
 
+if sys.version_info > (3,):
+    import queue as Queue
+else:
+    import Queue
+
 # Path
 path = os.path.join(os.path.dirname(__file__), os.pardir)
 sys.path.insert(0, os.path.abspath(path))
 
 #: python3 running
 PY3 = (sys.version_info > (3,))
+
+
+class TangoCB(object):
+
+    def __init__(self, queue):
+        self.__queue = queue
+
+    def push_event(self, *args, **kwargs):
+        event_data = args[0]
+        if event_data.err:
+            result = event_data.errors
+            print(result)
+        else:
+            result = event_data.attr_value.value
+        self.__queue.put(result)
 
 
 # Device test case
@@ -128,57 +148,11 @@ class LavueControllerTest(unittest.TestCase):
 
         self.proxy = None
 
-    def test_properties(self):
-        # test the properties
+    def test_property_DynamicROIs(self):
+        """ test the property DynamicROIs """
         print("Run: %s.%s() " % (
             self.__class__.__name__, sys._getframe().f_code.co_name))
 
-    def test_State(self):
-        """Test for State"""
-        print("Run: %s.%s() " % (
-            self.__class__.__name__, sys._getframe().f_code.co_name))
-        self.assertEqual(self.proxy.State(), PyTango.DevState.ON)
-
-    def test_Status(self):
-        """Test for Status"""
-        print("Run: %s.%s() " % (
-            self.__class__.__name__, sys._getframe().f_code.co_name))
-        self.assertEqual(self.proxy.Status(), 'State is ON')
-
-    def test_BeamCenterX(self):
-        """Test for BeamCenterX"""
-        print("Run: %s.%s() " % (
-            self.__class__.__name__, sys._getframe().f_code.co_name))
-        testvalues = [12.3, 123.2, -3.43, 0., 23423.]
-        for wvl in testvalues:
-            self.proxy.BeamCenterX = wvl
-            rvl = self.proxy.BeamCenterX
-            self.assertEqual(wvl, rvl)
-
-    def test_BeamCenterY(self):
-        """Test for BeamCenterY"""
-        print("Run: %s.%s() " % (
-            self.__class__.__name__, sys._getframe().f_code.co_name))
-        testvalues = [12.3, 123.2, -3.43, 0., 23423.]
-        for wvl in testvalues:
-            self.proxy.BeamCenterY = wvl
-            rvl = self.proxy.BeamCenterY
-            self.assertEqual(wvl, rvl)
-
-    def test_DetectorDistance(self):
-        """Test for DetectorDistance"""
-        print("Run: %s.%s() " % (
-            self.__class__.__name__, sys._getframe().f_code.co_name))
-        testvalues = [12.3, 123.2, -3.43, 0., 23423.]
-        for wvl in testvalues:
-            self.proxy.DetectorDistance = wvl
-            rvl = self.proxy.DetectorDistance
-            self.assertEqual(wvl, rvl)
-
-    def test_DetectorROIs(self):
-        """Test for DetectorROIs"""
-        print("Run: %s.%s() " % (
-            self.__class__.__name__, sys._getframe().f_code.co_name))
         testvalues = [
             ['{"Pilatus": [[61, 91, 83, 146], [332, 93, 382, 141], '
              '[116, 69, 279, 94], [91, 155, 157, 199]]}',
@@ -205,6 +179,260 @@ class LavueControllerTest(unittest.TestCase):
              [["ROI",
                [61, 91, 83, 146, 1, 21, 33, 146]]]],
         ]
+
+        db = PyTango.Database()
+        db.put_device_property(self.proxy.name(), {'DynamicROIs': True})
+        self.proxy.Init()
+
+        for wvl in testvalues:
+            self.proxy.DetectorROIs = str(wvl[0])
+            rvl = self.proxy.DetectorROIs
+            self.assertEqual(wvl[0], rvl)
+            for at, vl in wvl[1]:
+                self.assertTrue(hasattr(self.proxy, at))
+                rvl = self.proxy.read_attribute(at).value
+                self.assertTrue(np.array_equal(np.array(vl), rvl))
+
+        db.put_device_property(self.proxy.name(), {'DynamicROIs': False})
+        self.proxy.Init()
+
+        for wvl in testvalues:
+            self.proxy.DetectorROIs = str(wvl[0])
+            rvl = self.proxy.DetectorROIs
+            self.assertEqual(wvl[0], rvl)
+            for at, vl in wvl[1]:
+                self.assertTrue(not hasattr(self.proxy, at))
+
+        db.put_device_property(self.proxy.name(), {'DynamicROIs': True})
+        self.proxy.Init()
+
+        for wvl in testvalues:
+            self.proxy.DetectorROIs = str(wvl[0])
+            rvl = self.proxy.DetectorROIs
+            self.assertEqual(wvl[0], rvl)
+            for at, vl in wvl[1]:
+                self.assertTrue(hasattr(self.proxy, at))
+                rvl = self.proxy.read_attribute(at).value
+                self.assertTrue(np.array_equal(np.array(vl), rvl))
+
+    def test_property_DynamicROIsValues(self):
+        """ test the property DynamicROIsValues """
+        print("Run: %s.%s() " % (
+            self.__class__.__name__, sys._getframe().f_code.co_name))
+
+        testvalues = [
+            ['{"Pilatus": [1.23, 12.321, 83.323, 146.32]}',
+             [["PilatusSums",
+               [1.23, 12.321, 83.323, 146.32]]]],
+            ['{"Pilatus1": [1.23, 12.321, 83.323], '
+             '"Pilatus2": [146.32]}',
+             [["Pilatus1Sums",
+               [1.23, 12.321, 83.323]]],
+             [["Pilatus2Sum",
+               [146.32]]]],
+            ['{"lambda": [16.1]}',
+             [["LambdaSum",
+               [16.1]]]],
+            ['{"lambda2": [1231.61, 14.6]}',
+             [["Lambda2Sums",
+               [1231.61, 14.6]]]],
+            ['{"__null__": [12323.0]}',
+             [["Sum",
+               [12323.]]]],
+            ['{"__null__": [12312.0, 1232131.0]}',
+             [["Sums",
+               [12312., 1232131.]]]],
+        ]
+
+        db = PyTango.Database()
+        db.put_device_property(self.proxy.name(), {'DynamicROIsValues': True})
+        self.proxy.Init()
+
+        for wvl in testvalues:
+            self.proxy.DetectorROIsValues = str(wvl[0])
+            rvl = self.proxy.DetectorROIsValues
+            self.assertEqual(wvl[0], rvl)
+            for at, vl in wvl[1]:
+                rvl = self.proxy.read_attribute(at).value
+                if at.endswith("Sums"):
+                    self.assertTrue(np.array_equal(np.array(vl), rvl))
+                else:
+                    self.assertEqual(vl[0], rvl)
+
+        db.put_device_property(self.proxy.name(), {'DynamicROIsValues': False})
+        self.proxy.Init()
+
+        for wvl in testvalues:
+            self.proxy.DetectorROIsValues = str(wvl[0])
+            rvl = self.proxy.DetectorROIsValues
+            self.assertEqual(wvl[0], rvl)
+            for at, vl in wvl[1]:
+                self.assertTrue(not hasattr(self.proxy, at))
+
+        db.put_device_property(self.proxy.name(), {'DynamicROIsValues': True})
+        self.proxy.Init()
+
+        for wvl in testvalues:
+            self.proxy.DetectorROIsValues = str(wvl[0])
+            rvl = self.proxy.DetectorROIsValues
+            self.assertEqual(wvl[0], rvl)
+            for at, vl in wvl[1]:
+                rvl = self.proxy.read_attribute(at).value
+                if at.endswith("Sums"):
+                    self.assertTrue(np.array_equal(np.array(vl), rvl))
+                else:
+                    self.assertEqual(vl[0], rvl)
+
+    def test_property_ROIAttributesNames(self):
+        """ test the property ROIAttributesNames """
+        print("Run: %s.%s() " % (
+            self.__class__.__name__, sys._getframe().f_code.co_name))
+
+        testvalues = [
+            [[], True, True, []],
+            [["MyROI"], True, True, ["MyROI"]],
+            [["MySum"], True, True, ["MySum"]],
+            [["MySums"], True, True, ["MySums"]],
+            [["MySuma"], True, True, []],
+            [["YROI", "YSum", "YSums", "YSuma"], True, True,
+             ["YROI", "YSum", "YSums"]],
+            [[], False, False, []],
+            [["MyROI"], False, False, ["MyROI"]],
+            [["MySum"], False, False, ["MySum"]],
+            [["MySums"], False, False, ["MySums"]],
+            [["MySuma"], False, False, []],
+            [["YROI", "YSum", "YSums", "YSuma"], False, False,
+             ["YROI", "YSum", "YSums"]],
+        ]
+        db = PyTango.Database()
+
+        for wvl in testvalues:
+            db.put_device_property(
+                self.proxy.name(), {'ROIAttributesNames': wvl[0]})
+            db.put_device_property(self.proxy.name(), {'DynamicROIs': wvl[1]})
+            db.put_device_property(
+                self.proxy.name(), {'DynamicROIsValues': wvl[2]})
+            self.proxy.Init()
+            for at in wvl[3]:
+                self.assertTrue(hasattr(self.proxy, at))
+            for at in list(set(wvl[0]) - set(wvl[3])):
+                self.assertTrue(not hasattr(self.proxy, at))
+
+    def test_State(self):
+        """Test for State"""
+        print("Run: %s.%s() " % (
+            self.__class__.__name__, sys._getframe().f_code.co_name))
+        self.assertEqual(self.proxy.State(), PyTango.DevState.ON)
+
+    def test_Status(self):
+        """Test for Status"""
+        print("Run: %s.%s() " % (
+            self.__class__.__name__, sys._getframe().f_code.co_name))
+        self.assertEqual(self.proxy.Status(), 'State is ON')
+
+    def test_BeamCenterX(self):
+        """Test for BeamCenterX"""
+        print("Run: %s.%s() " % (
+            self.__class__.__name__, sys._getframe().f_code.co_name))
+        testvalues = [12.3, 123.2, -3.43, 0., 23423.]
+
+        queue = Queue.Queue()
+        cb = TangoCB(queue)
+        cb_id = self.proxy.subscribe_event(
+            "BeamCenterX", PyTango.EventType.CHANGE_EVENT, cb)
+        elem = queue.get(block=True, timeout=3)
+
+        for wvl in testvalues:
+            self.proxy.BeamCenterX = wvl
+            rvl = self.proxy.BeamCenterX
+            self.assertEqual(wvl, rvl)
+
+            elem = queue.get(block=True, timeout=3)
+            self.assertEqual(elem, wvl)
+
+        self.proxy.unsubscribe_event(cb_id)
+
+    def test_BeamCenterY(self):
+        """Test for BeamCenterY"""
+        print("Run: %s.%s() " % (
+            self.__class__.__name__, sys._getframe().f_code.co_name))
+        testvalues = [12.3, 123.2, -3.43, 0., 23423.]
+
+        queue = Queue.Queue()
+        cb = TangoCB(queue)
+        cb_id = self.proxy.subscribe_event(
+            "BeamCenterY", PyTango.EventType.CHANGE_EVENT, cb)
+        elem = queue.get(block=True, timeout=3)
+
+        for wvl in testvalues:
+            self.proxy.BeamCenterY = wvl
+            rvl = self.proxy.BeamCenterY
+            self.assertEqual(wvl, rvl)
+
+            elem = queue.get(block=True, timeout=3)
+            self.assertEqual(elem, wvl)
+
+        self.proxy.unsubscribe_event(cb_id)
+
+    def test_DetectorDistance(self):
+        """Test for DetectorDistance"""
+        print("Run: %s.%s() " % (
+            self.__class__.__name__, sys._getframe().f_code.co_name))
+        testvalues = [12.3, 123.2, -3.43, 0., 23423.]
+
+        queue = Queue.Queue()
+        cb = TangoCB(queue)
+        cb_id = self.proxy.subscribe_event(
+            "DetectorDistance", PyTango.EventType.CHANGE_EVENT, cb)
+        elem = queue.get(block=True, timeout=3)
+
+        for wvl in testvalues:
+            self.proxy.DetectorDistance = wvl
+            rvl = self.proxy.DetectorDistance
+            self.assertEqual(wvl, rvl)
+
+            elem = queue.get(block=True, timeout=3)
+            self.assertEqual(elem, wvl)
+
+        self.proxy.unsubscribe_event(cb_id)
+
+    def test_DetectorROIs(self):
+        """Test for DetectorROIs"""
+        print("Run: %s.%s() " % (
+            self.__class__.__name__, sys._getframe().f_code.co_name))
+
+        testvalues = [
+            ['{"Pilatus": [[61, 91, 83, 146], [332, 93, 382, 141], '
+             '[116, 69, 279, 94], [91, 155, 157, 199]]}',
+             [["PilatusROI",
+               [61, 91, 83, 146, 332, 93, 382, 141,
+                116, 69, 279, 94, 91, 155, 157, 199]]]],
+            ['{"Pilatus1": [[61, 91, 83, 146], [332, 93, 382, 141], '
+             '[116, 69, 279, 94]], "Pilatus2": [[91, 155, 157, 199]]}',
+             [["Pilatus1ROI",
+               [61, 91, 83, 146, 332, 93, 382, 141,
+                116, 69, 279, 94]]],
+             [["Pilatus2ROI",
+               [91, 155, 157, 199]]]],
+            ['{"lambda": [[61, 91, 83, 146]]}',
+             [["LambdaROI",
+               [61, 91, 83, 146]]]],
+            ['{"lambda2": [[61, 91, 83, 146], [1, 21, 33, 146]]}',
+             [["Lambda2ROI",
+               [61, 91, 83, 146, 1, 21, 33, 146]]]],
+            ['{"__null__": [[61, 91, 83, 146]]}',
+             [["ROI",
+               [61, 91, 83, 146]]]],
+            ['{"__null__": [[61, 91, 83, 146], [1, 21, 33, 146]]}',
+             [["ROI",
+               [61, 91, 83, 146, 1, 21, 33, 146]]]],
+        ]
+        queue = Queue.Queue()
+        cb = TangoCB(queue)
+        cb_id = self.proxy.subscribe_event(
+            "DetectorROIs", PyTango.EventType.CHANGE_EVENT, cb)
+        elem = queue.get(block=True, timeout=3)
+
         for wvl in testvalues:
             self.proxy.DetectorROIs = str(wvl[0])
             rvl = self.proxy.DetectorROIs
@@ -212,6 +440,11 @@ class LavueControllerTest(unittest.TestCase):
             for at, vl in wvl[1]:
                 rvl = self.proxy.read_attribute(at).value
                 self.assertTrue(np.array_equal(np.array(vl), rvl))
+
+                elem = queue.get(block=True, timeout=3)
+                self.assertEqual(elem, wvl[0])
+
+        self.proxy.unsubscribe_event(cb_id)
 
     def test_DetectorROIsValues(self):
         """Test for DetectorROIsValues"""
@@ -240,6 +473,13 @@ class LavueControllerTest(unittest.TestCase):
              [["Sums",
                [12312., 1232131.]]]],
         ]
+
+        queue = Queue.Queue()
+        cb = TangoCB(queue)
+        cb_id = self.proxy.subscribe_event(
+            "DetectorROIsValues", PyTango.EventType.CHANGE_EVENT, cb)
+        elem = queue.get(block=True, timeout=3)
+
         for wvl in testvalues:
             self.proxy.DetectorROIsValues = str(wvl[0])
             rvl = self.proxy.DetectorROIsValues
@@ -251,15 +491,31 @@ class LavueControllerTest(unittest.TestCase):
                 else:
                     self.assertEqual(vl[0], rvl)
 
+                elem = queue.get(block=True, timeout=3)
+                self.assertEqual(elem, wvl[0])
+
+        self.proxy.unsubscribe_event(cb_id)
+
     def test_Energy(self):
         """Test for Energy"""
         print("Run: %s.%s() " % (
             self.__class__.__name__, sys._getframe().f_code.co_name))
         testvalues = [12.3, 123.2, -3.43, 0., 23423.]
+
+        queue = Queue.Queue()
+        cb = TangoCB(queue)
+        cb_id = self.proxy.subscribe_event(
+            "Energy", PyTango.EventType.CHANGE_EVENT, cb)
+        elem = queue.get(block=True, timeout=3)
+
         for wvl in testvalues:
             self.proxy.Energy = wvl
             rvl = self.proxy.Energy
             self.assertEqual(wvl, rvl)
+            elem = queue.get(block=True, timeout=3)
+            self.assertEqual(wvl, elem)
+
+        self.proxy.unsubscribe_event(cb_id)
 
 
 def main():
