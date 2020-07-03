adb0b3819c397e217678dd8fc24afd891ec0161d
Jan Kotanski <jankotan@gmail.com>
2020-07-03 16:18:38 +0200

    update if statements

--- .travis/run.sh
+++ .travis/run.sh
@@ -7,7 +7,7 @@ else
     echo "run python3-lavue tests"
     docker exec -it ndts sh -c 'python3 test; status=$?; echo "Status: $status"; exit $status'
 fi
-if [ $? -ne "0" ]
+if [ "$?" -ne "0" ]
 then
     exit -1
 fi
