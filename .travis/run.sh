9c84f96478399fd0598f6cf1b239ec896c9cfeeb
Jan Kotanski <jankotan@gmail.com>
2020-07-03 15:23:36 +0200

    apply pep8

--- .travis/run.sh
+++ .travis/run.sh
@@ -5,7 +5,7 @@ if [ $1 = "2" ]; then
     docker exec -it ndts python test
 else
     echo "run python3-lavue tests"
-    docker exec -it ndts sh -c 'python3 test; status=$?; echo "Status: $status"; exit status'
+    docker exec -it ndts sh -c 'python3 test; status=$?; echo "Status: $status"; exit $status'
 fi
 if [ $? -ne "0" ]
 then
