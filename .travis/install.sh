b80522d479fa2fd8ffb9bb2875e28b56f169e997
Jan Kotanski <jankotan@gmail.com>
2020-07-13 10:36:52 +0200

    use new pyzmq buster package in tests

--- .travis/install.sh
+++ .travis/install.sh
@@ -29,12 +29,16 @@ docker exec -it --user root ndts service tango-starter restart
 
 if [ "$2" = "2" ]; then
     echo "install python packages"
-    docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y   python-pytango python-tz python-pyqtgraph python-setuptools python-zmq python-scipy  libzmq3-dev cython git'
-    docker exec -it --user root ndts /bin/sh -c 'git clone https://github.com/zeromq/pyzmq pyzmq-src'
-    docker exec -it --user root ndts /bin/sh -c 'cd pyzmq-src; python setup.py install'
+    if [ "$1" = "debian10" ]; then
+	docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y  python-tz python-pyqtgraph python-setuptools python-zmq python-scipy python-tango'
+    else
+	docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y   python-pytango python-tz python-pyqtgraph python-setuptools python-zmq python-scipy  libzmq3-dev cython git'
+	docker exec -it --user root ndts /bin/sh -c 'git clone https://github.com/zeromq/pyzmq pyzmq-src'
+	docker exec -it --user root ndts /bin/sh -c 'cd pyzmq-src; python setup.py install'
+    fi
 else
     echo "install python3 packages"
-    if [ "$1" = "ubuntu20.04" ]; then
+    if [ "$1" = "debian10" ] || [ "$1" = "ubuntu20.04" ] ; then
 	docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y  python3-tz python3-pyqtgraph python3-setuptools python3-zmq python3-scipy python3-tango'
     else
 	docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y  python3-tz python3-pyqtgraph python3-setuptools python3-zmq python3-scipy libzmq3-dev cython3  python3-pytango git'
