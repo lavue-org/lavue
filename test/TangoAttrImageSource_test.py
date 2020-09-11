e763f850461b77e305a210c20ad1c59c08943bad
Jan Kotanski <jankotan@gmail.com>
2020-09-11 17:48:55 +0200

    update tests

--- test/TangoAttrImageSource_test.py
+++ test/TangoAttrImageSource_test.py
@@ -481,7 +481,7 @@ class TangoAttrImageSourceTest(unittest.TestCase):
         self.compareStates(ls, dls,
                            ['viewrange', '__timestamp__', 'doordevice'])
 
-    def test_readspectra_color(self):
+    def ttest_readspectra_color(self):
         fun = sys._getframe().f_code.co_name
         print("Run: %s.%s() " % (self.__class__.__name__, fun))
 
@@ -1229,7 +1229,7 @@ class TangoAttrImageSourceTest(unittest.TestCase):
         self.compareStates(ls, dls,
                            ['viewrange', '__timestamp__', 'doordevice'])
 
-    def ttest_readtangofileimage(self):
+    def test_readtangofileimage(self):
         fun = sys._getframe().f_code.co_name
         print("Run: %s.%s() " % (self.__class__.__name__, fun))
 
@@ -1266,37 +1266,37 @@ class TangoAttrImageSourceTest(unittest.TestCase):
         qtck1 = QtChecker(app, dialog, True, sleep=100)
         qtck2 = QtChecker(app, dialog, True, sleep=100)
         qtck3 = QtChecker(app, dialog, True, sleep=100)
-        # qtck1.setChecks([
-        #     CmdCheck(
-        #         "_MainWindow__lavue._LiveViewer__sourcewg.isConnected"),
-        #     ExtCmdCheck(self, "getLavueState"),
-        #     CmdCheck(
-        #         "_MainWindow__lavue._LiveViewer__imagewg.rawData"),
-        #     CmdCheck(
-        #         "_MainWindow__lavue._LiveViewer__imagewg.currentData"),
-        #     ExtCmdCheck(self, "takeNewTangoFileImage"),
-        # ])
-        # qtck2.setChecks([
-        #     CmdCheck(
-        #         "_MainWindow__lavue._LiveViewer__sourcewg.isConnected"),
-        #     CmdCheck(
-        #         "_MainWindow__lavue._LiveViewer__imagewg.rawData"),
-        #     CmdCheck(
-        #         "_MainWindow__lavue._LiveViewer__imagewg.currentData"),
-        #     ExtCmdCheck(self, "takeNewTangoFileImage"),
-        # ])
-        # qtck3.setChecks([
-        #     CmdCheck(
-        #         "_MainWindow__lavue._LiveViewer__imagewg.rawData"),
-        #     CmdCheck(
-        #         "_MainWindow__lavue._LiveViewer__imagewg.currentData"),
-        #     WrapAttrCheck(
-        #         "_MainWindow__lavue._LiveViewer__sourcewg"
-        #         "._SourceTabWidget__sourcetabs[],0._ui.pushButton",
-        #         QtTest.QTest.mouseClick, [QtCore.Qt.LeftButton]),
-        #     CmdCheck(
-        #         "_MainWindow__lavue._LiveViewer__sourcewg.isConnected"),
-        # ])
+        qtck1.setChecks([
+            CmdCheck(
+                "_MainWindow__lavue._LiveViewer__sourcewg.isConnected"),
+            ExtCmdCheck(self, "getLavueState"),
+            CmdCheck(
+                "_MainWindow__lavue._LiveViewer__imagewg.rawData"),
+            CmdCheck(
+                "_MainWindow__lavue._LiveViewer__imagewg.currentData"),
+            ExtCmdCheck(self, "takeNewTangoFileImage"),
+        ])
+        qtck2.setChecks([
+            CmdCheck(
+                "_MainWindow__lavue._LiveViewer__sourcewg.isConnected"),
+            CmdCheck(
+                "_MainWindow__lavue._LiveViewer__imagewg.rawData"),
+            CmdCheck(
+                "_MainWindow__lavue._LiveViewer__imagewg.currentData"),
+            ExtCmdCheck(self, "takeNewTangoFileImage"),
+        ])
+        qtck3.setChecks([
+            CmdCheck(
+                "_MainWindow__lavue._LiveViewer__imagewg.rawData"),
+            CmdCheck(
+                "_MainWindow__lavue._LiveViewer__imagewg.currentData"),
+            WrapAttrCheck(
+                "_MainWindow__lavue._LiveViewer__sourcewg"
+                "._SourceTabWidget__sourcetabs[],0._ui.pushButton",
+                QtTest.QTest.mouseClick, [QtCore.Qt.LeftButton]),
+            CmdCheck(
+                "_MainWindow__lavue._LiveViewer__sourcewg.isConnected"),
+        ])
 
         print("execute")
         # qtck1.executeChecksAndClose(delay=3000)
@@ -1304,61 +1304,61 @@ class TangoAttrImageSourceTest(unittest.TestCase):
         qtck2.executeChecks(delay=6000)
         status = qtck3.executeChecksAndClose(delay=9000)
 
-        # self.assertEqual(status, 0)
+        self.assertEqual(status, 0)
 
-        # qtck1.compareResults(
-        #     self, [True, None, None, None, None], mask=[0, 0, 1, 1, 1])
-        # qtck2.compareResults(
-        #     self, [True, None, None, None], mask=[0, 1, 1, 1])
-        # qtck3.compareResults(
-        #     self, [None, None, None, False], mask=[1, 1, 0, 0])
+        qtck1.compareResults(
+            self, [True, None, None, None, None], mask=[0, 0, 1, 1, 1])
+        qtck2.compareResults(
+            self, [True, None, None, None], mask=[0, 1, 1, 1])
+        qtck3.compareResults(
+            self, [None, None, None, False], mask=[1, 1, 0, 0])
 
-        # res1 = qtck1.results()
-        # res2 = qtck2.results()
-        # res3 = qtck3.results()
-        # self.assertEqual(res1[2], None)
-        # self.assertEqual(res1[3], None)
-        # # self.assertTrue(np.allclose(res1[2], lastimage))
+        res1 = qtck1.results()
+        res2 = qtck2.results()
+        res3 = qtck3.results()
+        self.assertEqual(res1[2], None)
+        self.assertEqual(res1[3], None)
+        # self.assertTrue(np.allclose(res1[2], lastimage))
 
-        # # scaledimage = np.clip(lastimage, 10e-3, np.inf)
-        # # scaledimage = np.log10(scaledimage)
-        # # self.assertTrue(np.allclose(res1[3], scaledimage))
-
-        # lastimage = res1[4].T
-        # if not np.allclose(res2[1], lastimage):
-        #     print(res2[1])
-        #     print(lastimage)
-        # self.assertTrue(np.allclose(res2[1], lastimage))
         # scaledimage = np.clip(lastimage, 10e-3, np.inf)
         # scaledimage = np.log10(scaledimage)
-        # self.assertTrue(np.allclose(res2[2], scaledimage))
+        # self.assertTrue(np.allclose(res1[3], scaledimage))
 
-        # lastimage = res2[3].T
-        # self.assertTrue(np.allclose(res3[0], lastimage))
-        # scaledimage = np.clip(lastimage, 10e-3, np.inf)
-        # scaledimage = np.log10(scaledimage)
-        # self.assertTrue(np.allclose(res3[1], scaledimage))
+        lastimage = res1[4].T
+        if not np.allclose(res2[1], lastimage):
+            print(res2[1])
+            print(lastimage)
+        self.assertTrue(np.allclose(res2[1], lastimage))
+        scaledimage = np.clip(lastimage, 10e-3, np.inf)
+        scaledimage = np.log10(scaledimage)
+        self.assertTrue(np.allclose(res2[2], scaledimage))
 
-        # ls = json.loads(self.__lavuestate)
-        # dls = dict(self.__defaultls)
-        # dls.update(dict(
-        #     mode='expert',
-        #     source='tangofile',
-        #     configuration='test/testimageserver/00/LastImageTaken,'
-        #     'test/testimageserver/00/LastImagePath,{"/ramdisk/": "/gpfs/"}',
-        #     instance='tgtest',
-        #     tool='roi',
-        #     # log='debug',
-        #     log='info',
-        #     scaling='log',
-        #     levels='-20.0,20.0',
-        #     gradient='thermal',
-        #     tangodevice='test/lavuecontroller/00',
-        #     connected=True,
-        #     autofactor=None
-        # ))
-        # self.compareStates(ls, dls,
-        #                    ['viewrange', '__timestamp__', 'doordevice'])
+        lastimage = res2[3].T
+        self.assertTrue(np.allclose(res3[0], lastimage))
+        scaledimage = np.clip(lastimage, 10e-3, np.inf)
+        scaledimage = np.log10(scaledimage)
+        self.assertTrue(np.allclose(res3[1], scaledimage))
+
+        ls = json.loads(self.__lavuestate)
+        dls = dict(self.__defaultls)
+        dls.update(dict(
+            mode='expert',
+            source='tangofile',
+            configuration='test/testimageserver/00/LastImageTaken,'
+            'test/testimageserver/00/LastImagePath,{"/ramdisk/": "/gpfs/"}',
+            instance='tgtest',
+            tool='roi',
+            # log='debug',
+            log='info',
+            scaling='log',
+            levels='-20.0,20.0',
+            gradient='thermal',
+            tangodevice='test/lavuecontroller/00',
+            connected=True,
+            autofactor=None
+        ))
+        self.compareStates(ls, dls,
+                           ['viewrange', '__timestamp__', 'doordevice'])
 
 
 if __name__ == '__main__':
