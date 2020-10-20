a0c3bbb48234eca36369403e3ff23cc00f18370a
Jan Kotanski <jankotan@gmail.com>
2020-10-20 16:17:14 +0200

    change delay

--- test/CommandLineLavueState_test.py
+++ test/CommandLineLavueState_test.py
@@ -2209,7 +2209,7 @@ class CommandLineLavueStateTest(unittest.TestCase):
         dialog = lavuelib.liveViewer.MainWindow(options=options)
         dialog.show()
 
-        qtck = QtChecker(app, dialog, True, sleep=1000)
+        qtck = QtChecker(app, dialog, True, sleep=100)
         qtck.setChecks([
             CmdCheck(
                 "_MainWindow__lavue._LiveViewer__sourcewg.isConnected"),
@@ -2226,7 +2226,7 @@ class CommandLineLavueStateTest(unittest.TestCase):
                 "_MainWindow__lavue._LiveViewer__channelwg.rgbchannels"),
         ])
 
-        status = qtck.executeChecksAndClose()
+        status = qtck.executeChecksAndClose(delay=6000)
 
         self.assertEqual(status, 0)
         qtck.compareResults(self, [True, None, None, False, 10, (0, 1, 2)])
