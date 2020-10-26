6f4c611fea5c25fe3e36ca6bef2647647987acb9
Jan Kotanski <jankotan@gmail.com>
2020-10-26 08:46:34 +0100

    add groovy tests

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,13 +4,13 @@
 if [ "$1" = "ubuntu16.04" ]; then
     docker exec -it --user root ndts sed -i "s/\[mysqld\]/\[mysqld\]\nsql_mode = NO_ZERO_IN_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION/g" /etc/mysql/mysql.conf.d/mysqld.cnf
 fi
-if [ "$1" = "ubuntu20.04" ]; then
+if [ "$1" = "ubuntu20.04" ] || [ "$1" = "ubuntu20.04" ]; then
     docker exec -it --user root ndts sed -i "s/\[mysql\]/\[mysqld\]\nsql_mode = NO_ZERO_IN_DATE,NO_ENGINE_SUBSTITUTION\ncharacter_set_server=latin1\ncollation_server=latin1_swedish_ci\n\[mysql\]/g" /etc/mysql/mysql.conf.d/mysql.cnf
 fi
 
 # workaround for a bug in debian9, i.e. starting mysql hangs
 docker exec -it --user root ndts service mysql stop
-if [ "$1" = "ubuntu20.04" ]; then
+if [ "$1" = "ubuntu20.04" ] || [ "$1" = "ubuntu20.04" ]; then
     docker exec -it --user root ndts /bin/sh -c 'sudo usermod -d /var/lib/mysql/ mysql'
 fi
 docker exec -it --user root ndts /bin/sh -c '$(service mysql start &) && sleep 30'
@@ -35,7 +35,7 @@ if [ "$2" = "2" ]; then
 	docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y  python-tz python-pyqtgraph python-setuptools python-zmq python-scipy python-pytango'
 else
     echo "install python3 packages"
-    if [ "$1" = "debian10" ] || [ "$1" = "ubuntu20.04" ] || [ "$1" = "debian11" ] ; then
+    if [ "$1" = "debian10" ] || [ "$1" = "ubuntu20.04" ] || [ "$1" = "ubuntu20.10" ] || [ "$1" = "debian11" ] ; then
 	docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y  python3-tz python3-pyqtgraph python3-setuptools python3-zmq python3-scipy python3-tango'
     else
 	docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y  python3-tz python3-pyqtgraph python3-setuptools python3-zmq python3-scipy python3-pytango'
