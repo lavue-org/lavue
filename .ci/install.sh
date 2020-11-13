fd998e416a8076be322874cd228a9b0049c1dc9e
Jan Kotanski <jankotan@gmail.com>
2020-11-13 16:18:11 +0100

    remove /tmp/.X11-unix

--- .ci/install.sh
+++ .ci/install.sh
@@ -17,24 +17,23 @@ docker exec  --user root ndts /bin/bash -c '$(service mysql start &) && sleep 30
 
 
 docker exec  --user root ndts /bin/bash -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y tango-db tango-common; sleep 10'
-if [ "$?" -ne "0" ]
-then
-    exit -1
-fi
+if [ "$?" -ne "0" ]; then exit -1; fi
+
 docker exec  --user root ndts /bin/bash -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y xvfb  libxcb1 libx11-xcb1 libxcb-keysyms1 libxcb-image0 libxcb-icccm4 libxcb-render-util0 xkb-data'
-if [ "$?" -ne "0" ]
-then
-    exit -1
-fi
+if [ "$?" -ne "0" ]; then exit -1; fi
+
+docker exec  --user root ndts mkdir -p /tmp/runtime-tango
+docker exec  --user root ndts chown -R tango:tango /tmp/runtime-tango
+
+docker exec  --user root ndts /bin/bash -c 'rm -rf /tmp/.X11-unix/X1 && rm -rf /tmp/.X1'
+if [ "$?" -ne "0" ]; then exit -1; fi
 echo "start Xvfb :99 -screen 0 1024x768x24 &"
 docker exec  --user root ndts /bin/bash -c 'export DISPLAY=":99.0"; Xvfb :99 -screen 0 1024x768x24 &'
+if [ "$?" -ne "0" ]; then exit -1; fi
 
 echo "install tango servers"
 docker exec --user root ndts /bin/bash -c 'export DEBIAN_FRONTEND=noninteractive;  apt-get -qq update; apt-get -qq install -y  tango-starter tango-test liblog4j1.2-java pyqt5-dev-tools git'
-if [ "$?" -ne "0" ]
-then
-    exit -1
-fi
+if [ "$?" -ne "0" ]; then exit -1; fi
 
 docker exec  --user root ndts service tango-db restart
 docker exec  --user root ndts service tango-starter restart
@@ -50,23 +49,16 @@ else
 	docker exec  --user root ndts /bin/bash -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y  python3-tz python3-pyqtgraph python3-setuptools python3-zmq python3-scipy python3-pytango'
     fi
 fi
-if [ "$?" -ne "0" ]
-then
-    exit -1
-fi
+if [ "$?" -ne "0" ]; then exit -1; fi
 
+docker exec  --user root ndts chown -R tango:tango .
 if [ "$2" = "2" ]; then
     echo "install python-lavue"
-    docker exec  --user root ndts chown -R tango:tango .
     docker exec  --user root ndts python setup.py build
     docker exec  --user root ndts python setup.py install
 else
     echo "install python3-lavue"
-    docker exec  --user root ndts chown -R tango:tango .
     docker exec  --user root ndts python3 setup.py build
     docker exec  --user root ndts python3 setup.py install
 fi
-if [ "$?" -ne "0" ]
-then
-    exit -1
-fi
+if [ "$?" -ne "0" ]; then exit -1; fi
