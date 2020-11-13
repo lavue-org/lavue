7b901617734041076fed7746f8e03cdd4afaa4e5
Jan Kotanski <jankotan@gmail.com>
2020-11-13 09:00:07 +0100

    add display

--- .ci/run.sh
+++ .ci/run.sh
@@ -1,8 +1,9 @@
+	    docker exec ndts sh -c 'export DISPLAY=":99.0"; python3 test/__main__.py tangosource; status=$?; teststatus=$(cat "testresult.txt") && echo "Exit status: $status, Test Result: $teststatus" && exit $teststatus'
 #!/usr/bin/env bash
 
 if [ "$2" = "2" ]; then
     echo "run python-lavue tests"
-    docker exec ndts python test/__main__.py $1
+    docker exec ndts sh -c 'export DISPLAY=":99.0"; python test/__main__.py  '$1
 else
     echo "run python3-lavue tests"
     # workaround for pyfai docker problem, return I/O error status=74
@@ -24,10 +25,9 @@ else
 	    if [ "$?" -ne "0" ]; then exit -1; fi
 	    docker exec ndts sh -c 'export DISPLAY=":99.0"; python3 test/__main__.py controller2; status=$?; teststatus=$(cat "testresult.txt") && echo "Exit status: $status, Test Result: $teststatus" && exit $teststatus'
 	    if [ "$?" -ne "0" ]; then exit -1; fi
-	    docker exec ndts sh -c 'export DISPLAY=":99.0"; python3 test/__main__.py tangosource; status=$?; teststatus=$(cat "testresult.txt") && echo "Exit status: $status, Test Result: $teststatus" && exit $teststatus'
 	fi
     else
-	docker exec ndts python3 test/__main__.py $1
+	docker exec ndts sh -c 'export DISPLAY=":99.0"; python3 test/__main__.py  '$1
     fi
 fi
 if [ "$?" -ne "0" ]
