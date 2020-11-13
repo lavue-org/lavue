713cec7201498ca100429fee5d69a3fb4e45d36d
Jan Kotanski <jankotan@gmail.com>
2020-11-13 14:30:27 +0100

    fix syntax error

--- .ci/run.sh
+++ .ci/run.sh
@@ -13,8 +13,8 @@ else
     if [ "$?" -ne "0" ]; then exit -1; fi
     docker exec ndts sh -c 'export DISPLAY=":99.0"; python3 test/__main__.py controller2; status=$?; teststatus=$(cat "testresult.txt") && echo "Exit status: $status, Test Result: $teststatus" && exit $teststatus'
     if [ "$?" -ne "0" ]; then exit -1; fi
-    fi
 fi
+
 if [ "$?" -ne "0" ]
 then
     exit -1
