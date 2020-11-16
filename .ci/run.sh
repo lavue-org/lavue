b6440b7b301404008741b13c852e72cc85f03fb5
Jan Kotanski <jankotan@gmail.com>
2020-11-16 17:24:05 +0100

    split tests

--- .ci/run.sh
+++ .ci/run.sh
@@ -6,18 +6,29 @@ if [ "$1" = "2" ]; then
     if [ "$?" -ne "0" ]; then exit -1; fi
 else
     echo "run python3-lavue tests"
+    if [ "$2" = "basic" ]; then
     # workaround for pyfai docker problem, return I/O error status=74
-    docker exec ndts sh -c 'export DISPLAY=":99.0"; python3 test/__main__.py basic; status=$?; teststatus=$(cat "testresult.txt") && echo "Exit status: $status, Test Result: $teststatus" && exit $teststatus'
+	docker exec ndts sh -c 'export DISPLAY=":99.0"; python3 test/__main__.py basic; status=$?; teststatus=$(cat "testresult.txt") && echo "Exit status: $status, Test Result: $teststatus" && exit $teststatus'
+    fi
     if [ "$?" -ne "0" ]; then exit -1; fi
-    docker exec ndts sh -c 'export DISPLAY=":99.0"; python3 test/__main__.py controller; status=$?; teststatus=$(cat "testresult.txt") && echo "Exit status: $status, Test Result: $teststatus" && exit $teststatus'
+    if [ "$2" = "controller" ]; then
+	docker exec ndts sh -c 'export DISPLAY=":99.0"; python3 test/__main__.py controller; status=$?; teststatus=$(cat "testresult.txt") && echo "Exit status: $status, Test Result: $teststatus" && exit $teststatus'
+    fi
     if [ "$?" -ne "0" ]; then exit -1; fi
-    docker exec ndts sh -c 'export DISPLAY=":99.0"; python3 test/__main__.py controller2; status=$?; teststatus=$(cat "testresult.txt") && echo "Exit status: $status, Test Result: $teststatus" && exit $teststatus'
+    if [ "$2" = "controller2" ]; then
+	docker exec ndts sh -c 'export DISPLAY=":99.0"; python3 test/__main__.py controller2; status=$?; teststatus=$(cat "testresult.txt") && echo "Exit status: $status, Test Result: $teststatus" && exit $teststatus'
+    fi
     if [ "$?" -ne "0" ]; then exit -1; fi
-    docker exec ndts sh -c 'export DISPLAY=":99.0"; python3 test/__main__.py tangosuite; status=$?; teststatus=$(cat "testresult.txt") && echo "Exit status: $status, Test Result: $teststatus" && exit $teststatus'
+    if [ "$2" = "tangosource" ]; then
+	docker exec ndts sh -c 'export DISPLAY=":99.0"; python3 test/__main__.py tangosource; status=$?; teststatus=$(cat "testresult.txt") && echo "Exit status: $status, Test Result: $teststatus" && exit $teststatus'
+    fi
     if [ "$?" -ne "0" ]; then exit -1; fi
-fi
-
-if [ "$?" -ne "0" ]
-then
-    exit -1
+    if [ "$2" = "httpsource" ]; then
+	docker exec ndts sh -c 'export DISPLAY=":99.0"; python3 test/__main__.py httpsource; status=$?; teststatus=$(cat "testresult.txt") && echo "Exit status: $status, Test Result: $teststatus" && exit $teststatus'
+    fi
+    if [ "$?" -ne "0" ]; then exit -1; fi
+    if [ "$2" = "all" ]; then
+	docker exec ndts sh -c 'export DISPLAY=":99.0"; python3 test/__main__.py all; status=$?; teststatus=$(cat "testresult.txt") && echo "Exit status: $status, Test Result: $teststatus" && exit $teststatus'
+    fi
+    if [ "$?" -ne "0" ]; then exit -1; fi
 fi
