76c29018bb50fa6d8c85645d8ca79b27ecdda431
Jan Kotanski <jankotan@gmail.com>
2024-05-23 11:27:39 +0200

    replace deprecated tz.localize() with datetime.replace()

--- lavuelib/filewriter.py
+++ lavuelib/filewriter.py
@@ -532,7 +532,10 @@ class FTFile(FTObject):
             import tzlocal
             tz = tzlocal.get_localzone()
         fmt = '%Y-%m-%dT%H:%M:%S.%f%z'
-        starttime = tz.localize(datetime.datetime.now())
+        if sys.version_info > (3, 6):
+            starttime = datetime.datetime.now().replace(tzinfo=tz)
+        else:
+            starttime = tz.localize(datetime.datetime.now())
         return str(starttime.strftime(fmt))
 
     def default_field(self):
