e28be685fc3ed4a1aaaaf3e30ab101e35af6d9cb
Jan Kotanski <jankotan@gmail.com>
2024-12-03 10:23:40 +0100

    change sidebarwidth

--- doc/conf.py
+++ doc/conf.py
@@ -205,6 +205,7 @@ html_theme_options = {
     # Choose Bootstrap version.
     # Values: "3" (default) or "2" (in quotes)
     # 'bootstrap_version': "3",
+    "sidebarwidth":400,
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
