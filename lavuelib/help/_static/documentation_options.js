18a28feaa9aaa71155d91c306d86908169b90700
Jan Kotanski <jankotan@gmail.com>
2024-12-16 10:14:23 +0100

    update help

--- lavuelib/help/_static/documentation_options.js
+++ lavuelib/help/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.93.1',
+    VERSION: '2.94.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
