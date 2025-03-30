3aad9e76b05ae18a4a33f00231eb270975e1bc5a
Jan Kotanski <jankotan@gmail.com>
2025-03-30 10:37:09 +0200

    add support for pyqtgraph 0.11.0

--- lavuelib/imageWidget.py
+++ lavuelib/imageWidget.py
@@ -254,27 +254,12 @@ class ImageWidget(QtWidgets.QWidget):
         self.__userimageplot = _pg.PlotWidget(self)
         self.__userimage = _pg.ImageItem()
         self.__userimageplot.getViewBox().addItem(self.__userimage)
-        # # does not work for older pyqtgraph e.g. deb11
-        # self.__userimagecolorbar = self.__userimageplot.addColorBar(
-        #     self.__userimage, colorMap='inferno')
 
-        if hasattr(_pg, "colormap") and hasattr(_pg.colormap, "get"):
-            colorMap = _pg.colormap.get('inferno')
+        if hasattr(self.__userimageplot, "addColorBar"):
+            self.__userimagecolorbar = self.__userimageplot.addColorBar(
+                self.__userimage, colorMap='inferno')
         else:
-            colors = [
-                (0, 0, 0),
-                (45, 5, 61),
-                (84, 42, 55),
-                (150, 87, 60),
-                (208, 171, 141),
-                (255, 255, 255)
-            ]
-            colorMap = _pg.ColorMap(
-                pos=np.linspace(0.0, 1.0, 6), color=colors)
-
-        self.__userimagecolorbar = _pg.ColorBarItem(colorMap=colorMap)
-        self.__userimagecolorbar.setImageItem(
-            self.__userimage, insert_in=self.__userimageplot.plotItem)
+            self.__userimagecolorbar = None
 
         # self.__userimagecolorbar.setColorMap('viridis')
         # self.__userimagecolorbar.setLevels(values=None, low=None, high=None)
