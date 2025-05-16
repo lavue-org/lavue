2efc0bc4d1b01bee03d99aa413a427c787d3ca58
Jan Kotanski <jankotan@gmail.com>
2025-05-16 20:27:29 +0200

    fix timezones

--- lavuelib/filewriter.py
+++ lavuelib/filewriter.py
@@ -526,6 +526,8 @@ class FTFile(FTObject):
         :rtype: :obj:`str`
         """
         tzone = time.tzname[0]
+        if tzone in ['CET', 'CEST']:
+            tzone = 'Europe/Berlin'
         fmt = '%Y-%m-%dT%H:%M:%S.%f%z'
         try:
             if sys.version_info >= (3, 9):
