7c1a133fa763bf63f5aa893bc0ea8451369bf93c
Jan Kotanski <jankotan@gmail.com>
2025-07-07 08:35:44 +0200

    bump the version

--- lavuelib/help/_static/bizstyle.js
+++ lavuelib/help/_static/bizstyle.js
@@ -23,7 +23,7 @@ const initialiseBizStyle = () => {
 }
 
 window.addEventListener("resize",
-  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "LaVue 3.3.1 documentation")
+  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "LaVue 3.3.2 documentation")
 )
 
 if (document.readyState !== "loading") initialiseBizStyle()
