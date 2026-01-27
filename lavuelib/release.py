247f59d3f22adfcfd353c0668fbfafde836058ca
Jan Kotanski <jankotan@gmail.com>
2026-01-27 14:25:26 +0100

    add support for pyfai 2025.03 (#909)

--- lavuelib/release.py
+++ lavuelib/release.py
@@ -26,5 +26,5 @@
 """ release version """
 
 #: (:obj:`str`) the live viewer version
-__version__ = "3.6.0"
+__version__ = "3.7.0"
 # __version__ = "3.5"
--- lavuelib/settings.py
+++ lavuelib/settings.py
@@ -995,7 +995,10 @@ class Settings(object):
 
             with QtCore.QMutexLocker(self.aimutex):
                 if wvln and detdistance and pixel1 and pixel2:
-                    from pyFAI.azimuthalIntegrator import AzimuthalIntegrator
+                    try:
+                        from pyFAI.integrator.azimuthal import AzimuthalIntegrator
+                    except Exception:
+                        from pyFAI.azimuthalIntegrator import AzimuthalIntegrator
                     self.ai = AzimuthalIntegrator(
                         dist=detdistance,
                         poni1=self.detponi1,
