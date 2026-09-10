51a39c3e8ff106db908d3b2c79eab3a7c130211e
Jan Kotanski <jankotan@gmail.com>
2026-09-10 20:13:51 +0200

    Update run_pixi.sh

--- .ci/run_pixi.sh
+++ .ci/run_pixi.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "install pixi"
-command='source .sh.sh ;  QT_QPA_PLATFORM=offscreen python3 test/__main__.py $1; status=$?; teststatus=$(cat "testresult.txt") && echo "Exit status: $status, Test Result: $teststatus" && exit $teststatus'
+command='source .sh.sh ;  QT_QPA_PLATFORM=offscreen python3 test/__main__.py '$1'; status=$?; teststatus=$(cat "testresult.txt") && echo "Exit status: $status, Test Result: $teststatus" && exit $teststatus'
 echo "$command"
 docker exec ndts bash -c "$command"
 if [ "$?" != "0" ]; then exit 255; fi
